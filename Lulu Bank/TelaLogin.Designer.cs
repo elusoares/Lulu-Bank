9b609b270fad89e6bfea6510c87416bc8db83806
elusoares <elusaisoares@gmail.com>
2018-01-03 21:04:11 -0300

    Adicionar arquivos de projeto.

---
+++ Lulu Bank/TelaLogin.Designer.cs
@@ -0,0 +1,173 @@
+﻿namespace Lulu_Bank
+{
+    partial class TelaLogin
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBoxUsuario = new System.Windows.Forms.TextBox();
+            this.textBoxSenha = new System.Windows.Forms.TextBox();
+            this.labelUsuario = new System.Windows.Forms.Label();
+            this.labelSenha = new System.Windows.Forms.Label();
+            this.buttonLogin = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBoxDinheiro = new System.Windows.Forms.PictureBox();
+            this.labelTitle = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxDinheiro)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBoxUsuario
+            // 
+            this.textBoxUsuario.Location = new System.Drawing.Point(72, 50);
+            this.textBoxUsuario.Name = "textBoxUsuario";
+            this.textBoxUsuario.Size = new System.Drawing.Size(163, 20);
+            this.textBoxUsuario.TabIndex = 0;
+            // 
+            // textBoxSenha
+            // 
+            this.textBoxSenha.Location = new System.Drawing.Point(72, 84);
+            this.textBoxSenha.Name = "textBoxSenha";
+            this.textBoxSenha.Size = new System.Drawing.Size(163, 20);
+            this.textBoxSenha.TabIndex = 1;
+            // 
+            // labelUsuario
+            // 
+            this.labelUsuario.AutoSize = true;
+            this.labelUsuario.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.labelUsuario.Location = new System.Drawing.Point(17, 53);
+            this.labelUsuario.Name = "labelUsuario";
+            this.labelUsuario.Size = new System.Drawing.Size(46, 13);
+            this.labelUsuario.TabIndex = 2;
+            this.labelUsuario.Text = "Usuário:";
+            // 
+            // labelSenha
+            // 
+            this.labelSenha.AutoSize = true;
+            this.labelSenha.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.labelSenha.Location = new System.Drawing.Point(17, 87);
+            this.labelSenha.Name = "labelSenha";
+            this.labelSenha.Size = new System.Drawing.Size(41, 13);
+            this.labelSenha.TabIndex = 3;
+            this.labelSenha.Text = "Senha:";
+            // 
+            // buttonLogin
+            // 
+            this.buttonLogin.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.buttonLogin.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.buttonLogin.FlatAppearance.BorderSize = 0;
+            this.buttonLogin.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonLogin.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.buttonLogin.ImageAlign = System.Drawing.ContentAlignment.TopLeft;
+            this.buttonLogin.Location = new System.Drawing.Point(88, 133);
+            this.buttonLogin.Name = "buttonLogin";
+            this.buttonLogin.Size = new System.Drawing.Size(75, 23);
+            this.buttonLogin.TabIndex = 4;
+            this.buttonLogin.Text = "LOGIN";
+            this.buttonLogin.UseVisualStyleBackColor = false;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Teal;
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.textBoxUsuario);
+            this.panel1.Controls.Add(this.buttonLogin);
+            this.panel1.Controls.Add(this.labelUsuario);
+            this.panel1.Controls.Add(this.labelSenha);
+            this.panel1.Controls.Add(this.textBoxSenha);
+            this.panel1.Location = new System.Drawing.Point(68, 188);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(250, 185);
+            this.panel1.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Palatino Linotype", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.label1.Location = new System.Drawing.Point(85, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(82, 17);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Bem vindo(a)";
+            // 
+            // pictureBoxDinheiro
+            // 
+            this.pictureBoxDinheiro.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBoxDinheiro.Image = global::Lulu_Bank.Properties.Resources.dinheiro1;
+            this.pictureBoxDinheiro.Location = new System.Drawing.Point(156, 68);
+            this.pictureBoxDinheiro.Name = "pictureBoxDinheiro";
+            this.pictureBoxDinheiro.Size = new System.Drawing.Size(75, 60);
+            this.pictureBoxDinheiro.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBoxDinheiro.TabIndex = 6;
+            this.pictureBoxDinheiro.TabStop = false;
+            this.pictureBoxDinheiro.Click += new System.EventHandler(this.pictureBoxDinheiro_Click);
+            // 
+            // labelTitle
+            // 
+            this.labelTitle.AutoSize = true;
+            this.labelTitle.Font = new System.Drawing.Font("Palatino Linotype", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelTitle.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.labelTitle.Location = new System.Drawing.Point(123, 26);
+            this.labelTitle.Name = "labelTitle";
+            this.labelTitle.Size = new System.Drawing.Size(157, 39);
+            this.labelTitle.TabIndex = 7;
+            this.labelTitle.Text = "Lulu Bank";
+            // 
+            // TelaLogin
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DarkSlateGray;
+            this.ClientSize = new System.Drawing.Size(385, 415);
+            this.Controls.Add(this.labelTitle);
+            this.Controls.Add(this.pictureBoxDinheiro);
+            this.Controls.Add(this.panel1);
+            this.Name = "TelaLogin";
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxDinheiro)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBoxUsuario;
+        private System.Windows.Forms.TextBox textBoxSenha;
+        private System.Windows.Forms.Label labelUsuario;
+        private System.Windows.Forms.Label labelSenha;
+        public System.Windows.Forms.Button buttonLogin;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.PictureBox pictureBoxDinheiro;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label labelTitle;
+    }
+}
+
---
+++ Lulu Bank/TelaLogin.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lulu_Bank
+{
+    public partial class TelaLogin : Form
+    {
+        public TelaLogin()
+        {
+            InitializeComponent();
+            this.MaximizeBox = false;
+            this.Text = "Lulu Bank";
+            //Bitmap icon = new Bitmap("Resources/dinheiro.png");
+            this.Icon = Icon.FromHandle(Properties.Resources.dinheiro.GetHicon());
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Conta conta1 = new Conta();
+            conta1.numero = 1;
+            conta1.titular = "Joaquim Jose";
+            conta1.saldo = 20000;
+        }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBoxDinheiro_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
